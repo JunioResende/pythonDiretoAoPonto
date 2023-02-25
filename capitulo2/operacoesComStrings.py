b62bb1ffca1a6f425951fa90ec509983231ead99
JunioResende <JunioResende>
2023-02-25 09:36:58 -0300

    operacoes com strings

---
+++ capitulo2/operacoesComStrings.py
@@ -0,0 +1,16 @@
+nome = "Junio"
+sobrenome = "Resende"
+
+print(nome + sobrenome)
+
+#Calculo da area de um circulo
+raio = 10
+pi = 3.14
+
+print("Area do circulo " + str(pi * raio ** 2))
+
+#Calculo area de um retangulo
+base = 10
+altura = 20
+
+print("Area do retangulo " + str(base*altura))
