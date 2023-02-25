3903fae296e268154876173867fa588c6fb9848d
JunioResende <JunioResende>
2023-02-25 12:10:58 -0300

    expressoes booleanas

---
+++ capitulo3/expressoesBooleanas.py
@@ -0,0 +1,28 @@
+#Comparacao de igualdade
+print("Comparacao de igualdade")
+num1 = input("Digite um valor a ser comparado ")
+num2 = input("Digite o outro valor da comparacao ")
+print("A comparacao e", num1 == num2)
+print("********************************************")
+
+
+#Comparacao de desigualdade
+print("Comparacao de desigualdade")
+num1 = input("Digite um valor a ser comparado ")
+num2 = input("Digite o outro valor da comparacao ")
+print("A comparacao e", num1 != num2)
+print("********************************************")
+
+#Comparacao maior que
+print("Comparacao maior que")
+num1 = input("Digite um valor a ser comparado ")
+num2 = input("Digite o outro valor da comparacao ")
+print("A comparacao e", num1 > num2)
+print("********************************************")
+
+#Comparacao menor que
+print("Comparacao menor que")
+num1 = input("Digite um valor a ser comparado ")
+num2 = input("Digite o outro valor da comparacao ")
+print("A comparacao e", num1 < num2)
+print("********************************************")
