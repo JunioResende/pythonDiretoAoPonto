a2aaebbaa3be60b2f9db618a0ec8776c9b7bbc00
JunioResende <JunioResende>
2023-02-25 18:33:35 -0300

    if

---
+++ capitulo3/operadoresBooleanos.py
@@ -0,0 +1,29 @@
+#Operador and
+"""O operador and avalia se dois resultados da expressao sao verdadeiros. Se os dois valores forem verdadeiros
+o retorno sera true. Caso contrario retornara false
+"""
+
+print("***Operador and***")
+
+num1 = input("digite o valor 1 da comparacao")
+num2 = input("digite o valor 2 da comparacao")
+num3 = input("digite o valor 3 da comparacao")
+num4 = input("digite o valor 4 da comparacao")
+
+print("O valor da comparacao e", num1 == num2 and num3 > num4)
+
+print("*********************************************************")
+
+#Operador or
+#O operador or avalia se apenas um dos resultados da expressao e verdadeiro
+
+print("***Operador or***")
+
+num1 = input("digite o valor 1 da comparacao")
+num2 = input("digite o valor 2 da comparacao")
+num3 = input("digite o valor 3 da comparacao")
+num4 = input("digite o valor 4 da comparacao")
+
+print("O valor da comparacao e", num1 == num2 and num3 > num4)
+
+print("*********************************************************")
