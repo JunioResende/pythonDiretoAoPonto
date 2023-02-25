5654ff9c62881b287ff77b6851cd47918b218eb6
JunioResende <JunioResende>
2023-02-25 20:15:13 -0300

    while

--- capitulo4/While.py
+++ capitulo4/While.py
@@ -4,4 +4,24 @@ while x < 10:
     print(x)
     x = x + 1
 
-#Loop infinito e break
+# Loop infinito e break
+capital = "Ottawa"
+while True:
+    resposta = input("Qual a capital do canada? ")
+    if resposta == capital:
+        print("Certa resposta!")
+        break
+    else:
+        print("Resposta incorreta. Tente Novamente!")
+
+# Continue
+while True:
+    username = input("username? ")
+    if username != "neo":
+        print("Nunca nem vi")
+        continue
+    senha = input("Qual a senha? ")
+    if senha == "1234":
+        print("Bem vindo, {}".format(username))
+        print("Aceita um cafe")
+        break
