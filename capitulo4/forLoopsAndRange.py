92c1b293f9cb10cff414ceb9b35251c4c12581d0
JunioResende <JunioResende>
2023-02-28 21:42:34 -0300

    for

---
+++ capitulo4/forLoopsAndRange.py
@@ -0,0 +1,6 @@
+"""O for e a estrutura de repeticao mais popular do python, o motivo disso e que ele e muito conveniene para percorrer
+os objetos iteraveis, como as listas. Vejamos como contar de 0 a 5 com o for.
+"""
+
+for i in range(6): # O range faz a contagem iniciando pelo 0 []
+    print(i)
