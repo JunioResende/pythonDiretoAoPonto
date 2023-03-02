0f4180b5637baab2fe33a7e8e319d7a94f9d4c1d
JunioResende <JunioResende>
2023-03-02 15:03:04 -0300

    mais sobre dicionarios

--- capitulo4/dicionarios.py
+++ capitulo4/dicionarios.py
@@ -1,3 +1,17 @@
 cliente = {"nome": "Junio", "idade": 32, "sexo": "masculino"}
 
 print(cliente["nome"])
+
+print("**********************************************************")
+
+skills = {"sabedoria": 8, "coragem": 7, "força": 9}
+
+for sk in skills:
+    print(sk)
+
+print("**********************************************************")
+
+for sk, values in skills.items():
+    print(sk, values)
+
+print("**********************************************************")
