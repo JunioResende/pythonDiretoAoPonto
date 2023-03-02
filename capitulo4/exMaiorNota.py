fbbb2f9f55402af57d834ad5d04f6d1ca58a40d3
JunioResende <JunioResende>
2023-03-02 18:46:42 -0300

    ex mior nota

---
+++ capitulo4/exMaiorNota.py
@@ -0,0 +1,13 @@
+notas = [1, 4, 6, 10, 8, 5, 7, 3, 9, 2]
+
+maiorNota = notas[0]
+
+for n in notas :
+    if n > maiorNota:
+        maiorNota = n
+print(maiorNota)
+
+print("********************************************************")
+
+
+
